9fc34e18b16e6702e7556b06bb6342a497aaa6d7
Ram Lakhan Jangid <rljangid825@gmail.com>
2026-07-31 15:34:18 +0530

    Change API base URL to production server

--- frontend/lib/apiClient.ts
+++ frontend/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://three-way-match-engine-all0.onrender.com',
 });
 
 apiClient.interceptors.request.use((config) => {
